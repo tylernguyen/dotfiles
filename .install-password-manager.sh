6a67adda8269cfa1db01026ef77e587efc0b8a14
Tyler Nguyen <human@tylernguyen.email>
2024-06-05 18:03:49 -0500

    chore: Initial commit

---
+++ .install-password-manager.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+type password-manager-binary >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    # commands to install password-manager-binary on Darwin
+    brew install --cask 1password
+    ;;
+Linux)
+    # commands to install password-manager-binary on Linux
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
